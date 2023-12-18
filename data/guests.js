9e58af6bd5667652bcb4b7f8e31cafbacd7d950d
RndmCodeGuy20 <manesg@rknec.edu>
2023-12-18 11:19:54 +0530

    Add Pratik Agrawal to the list of guests

--- data/guests.js
+++ data/guests.js
@@ -3,10 +3,10 @@ export const guests = [
         name: "Shantanu Mane",
         github: "https://github.com/RndmCodeGuy20",
     },
-    // {
-    //     name: "Pratik Agrawal",
-    //     github: "https://github.com/RudraPratik30",
-    // },
+    {
+        name: "Pratik Agrawal",
+        github: "https://github.com/RudraPratik30",
+    },
     {
         name: "Abhishek Deshpande",
         github: "https://github.com/deshabhishek007",
